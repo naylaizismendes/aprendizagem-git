f79af629b8bb8d91d8df0ff3284aa21d5a50e762
naylaizismendes <114931127+naylaizismendes@users.noreply.github.com>
2023-06-21 16:30:08 -0300

    criando

--- AloMundo.java
+++ AloMundo.java
@@ -1,4 +1,4 @@
- public class Main{
+ public class AloMundo{
 public static void main(String args[]){
 System.out.println("Nayla IZIS MENDES FERREIRA!");
 }
---
+++ logica.java
@@ -0,0 +1,16 @@
+import javax.naming.event.NamingExceptionEvent;
+
+public class logica {
+    public static void main(String[]args){
+    int a, b;
+    double resultado;
+    a=10;
+    b=5;
+    resultado = a/b;
+    System.out.println(resultado);
+
+    
+}
+
+
+}
